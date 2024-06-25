b4434ec783563ca732734ca61a8a2680a5b7875f
DianyLiz <dlenamorado.uth.hn>
2024-06-25 16:32:39 -0600

    Ejercicios Realizados el 25 de Junio 2024

---
+++ 000ClasesPrueba.py
@@ -0,0 +1,8 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+from ClasesEspeciales import Libro
+
+libro1 = Libro("Stephen King","It", 1032)
+print(str(libro1))
+print(len(libro1))
---
+++ 01Polimorfismo.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+class Perro:
+    def hablar(self):
+        print("Guau!")
+class Gato:
+    def hablar(self):
+        print("Miau!")
+hachiko = Perro()
+garfield = Gato()
+animales = [hachiko, garfield]
+for animal in [hachiko, garfield]:
+    animal.hablar()
